78a3706aeaa53964312991deb342ec71728bec97
Tommy McGinn <tmcginn1@nd.edu>
2020-12-04 02:23:32 -0600

    The documentation file of the package.

---
+++ R/PERFORMANCESCIENCERELIABILITY.R
@@ -0,0 +1,10 @@
+#' @title PerfSciReliability
+#'
+#' @description A package for computing various measures pertaining to reliability of performance science metrics. It contains
+#'   functions for the Typical Error (TE), Coefficient of Variation (CV), Smallest Worthwhile Change (SWC), Minimal Detectable Change
+#'   (MDC), Limits of Agreement (LoA), Standard Error of Measurement (SEM), Intraclass Correlation Coefficient (ICC), and Reliable
+#'   Change Index (RCI).
+#'
+#' @docType package
+#' @name PerfSciReliability
+NULL
