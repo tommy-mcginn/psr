9e411f0c0c29e6e270202fd15c9758af73049903
Tommy McGinn <tmcginn1@nd.edu>
2021-02-12 15:32:05 -0500

    Edits to functions and vignette.

--- R/PROBRC.R
+++ R/PROBRC.R
@@ -23,64 +23,71 @@
 #' metric_1 <- c(257, 268, 237, 275, 259, 263, 216, 287, 250)
 #' metric_2 <- c(1.11, 1.24, 0.89, 1.37, 1.21, 1.30, 0.75, 1.42, 1.15)
 #' metric_3 <- c(1272, 1493, 1072, 1046, 1198, 1165, 1478, 1370, 1335)
-#' prel(subject, trial, metric_1, metric_2, metric_3, ICC = c(0.92, 0.98, 0.95), initial = which(trial == "Trial 1"), final = which(trial == "Trial 2"))
+#' PROBRC(subject, trial, metric_1, metric_2, metric_3, ICC = c(0.92, 0.98, 0.95), initial = which(trial == "Trial 1"),
+#' final = which(trial == "Trial 2"))
 #'
 #' @references Ferger, K., & Büsch, D. (2018). Individual measurement of performance change in sports. Deutsche Zeitschrift Für
 #'   Sportmedizin, 2018(02), 45-52.
 #'
 #' @export
-prel <- function(subject, trial, ..., ICC, initial, final) {
+PROBRC <- function(subject, trial, ..., ICC, initial, final) {
 
-  # The individual subject and metric vectors that were passed to this function are brought together into a data frame
-  df <- data.frame(subject, ...)
+  # The inputs to this function are individual vectors, so I bring them all together into one data frame
+  input_df <- data.frame(subject, ...)
 
   # Calls the check_error function, which produces informative error messages if any of a variety of errors are made by the user
   check_error(subject, trial, ...)
 
   # We create separate data frames for the "initial trial" and "final trial" measurements, respectively
-  initial_df <- df[c(initial), ]
-  final_df <- df[c(final), ]
+  initial_df <- input_df[c(initial), ]
+  final_df <- input_df[c(final), ]
 
-  # This data frame is created from the get-go, and it will be the output that is returned at the end
-  output_df <- data.frame(Metric = paste("P(Reliable Change)"))
+  # A new data frame is created to store the probability that each final measurement was a reliable change from the initial measurement
+  output_df <- data.frame(subject)
 
-  #For each column except that of the subjects in the data, we want to compute the between-athlete sd
+  # For each column except that of the subjects in the data, we want to compute the between-athlete sd
   SD_baseline <- lapply(initial_df[, -1], stats::sd)
 
-  #Converting the ICC to a list (as opposed to a vector) makes the next for loop easier to calculate
+  # Converting the ICC to a list (as opposed to a vector) makes the next for loop easier to calculate
   ICC <- as.list(ICC)
 
-  #This makes a list for the SED values upfront, so that we can append to it in the next for loop
+  # This makes a list for the SED values upfront, so that we can append to it in the next for loop
   list_SED <- list()
 
-  #The sequence along SD_baseline is really just the number of metrics that were entered into the function arguments
+  # The sequence along SD_baseline is really just the number of metrics that were entered into the function arguments
   for (i in seq_along(SD_baseline)) {
 
-    #The SED is computed for each metric, and then its values are appended into the list we created earlier
+    # The SED is computed for each metric, and then its values are appended into the list we created earlier
     SED = SD_baseline[[i]] * sqrt(2) * (1 - ICC[[i]])
     list_SED <- append(list_SED, values = SED)
 
   }
 
-  for (i in 2:ncol(df)) {
+  for (i in 2:ncol(input_df)) {
 
-    #The change to be analyzed is the difference of the athlete's final score and initial score
+    # The change to be analyzed is the difference of the athlete's final score and initial score
     change = final_df[, i] - initial_df[, i]
 
-    #This change is divided by the standard error of difference of its metric, which is what that athlete's RCI is
+    # This change is divided by the standard error of difference of its metric, which is what that athlete's RCI is
     RCI = change / list_SED[[i - 1]]
 
-    #The probability of interest comes from a two-tailed test of the Student's t-distribution
-    prob = abs((2 * stats::pnorm(RCI)) - 1)
+    # The probability of interest comes from a two-tailed test of the Student's t-distribution
+    prob = round(abs((2 * stats::pnorm(RCI)) - 1), 2)
 
-    #Puts these vectors of probability values into a list and names each item of the list according to the metric it represents
+    # Puts these vectors of probability values into a list and names each item of the list according to the metric it represents
     prob_list <- list(prob)
     output_df <- cbind(output_df, unlist(prob_list))
-    colnames(output_df)[i] <- colnames(df)[i]
 
   }
 
-  #Explicitly prints the list to see its full contents
+  # The column names of the output data frame are assigned to be the metric names they represent
+  for (i in 2:ncol(output_df)) {
+
+    colnames(output_df)[i] <- colnames(input_df)[i]
+
+  }
+
+  # I print the data frame in this way as the output so that I can hide the "1" that otherwise appears as the row number
   print.data.frame(output_df, row.names = FALSE)
 
 }
