ce4d755ce7b70289d8d8c9e6ee483266bb52900e
Tommy McGinn <tmcginn1@nd.edu>
2021-02-22 01:30:13 -0500

    Some changes to file names and deleting a function we had discarded earlier.

---
+++ R/psr.R
@@ -0,0 +1,10 @@
+#' @title psr
+#'
+#' @description A package for computing various measures relating to reliability of performance science metrics. It
+#'   contains functions to compute the Typical Error (TE), Coefficient of Variation (CV), Standard Error of Measurement
+#'   (SEM), Smallest Worthwhile Change (SWC), Minimal Detectable Change (MDC), Intraclass Correlation Coefficient (ICC),
+#'   Standard Ten Scores (STEN), and Probability of a Reliable Change (PROBRC).
+#'
+#' @docType package
+#' @name psr
+NULL
