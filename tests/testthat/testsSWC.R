4f40b5be953a86a24c466bcfec7db80368d00892
Tommy McGinn <tmcginn1@nd.edu>
2021-03-07 23:54:21 -0500

    Made changes to alignment of package and added additional things necessary for submission to CRAN.

--- tests/testthat/testsSWC.R
+++ tests/testthat/testsSWC.R
@@ -10,9 +10,9 @@ trial_2 <- c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 metric_2 <- c(5.25, 5.24, 5.52, 5.63, 5.39, 5.39, 5.16, 5.5, 5.07, 5.32, 5.41, 5.25, 5.16, 5.31, 5.14)
 
 testthat::test_that("SWC", {
-  testthat::expect_equal(round(SWC(subject_1, trial_1, metric_1), 2)[1, 1], 0.09)
+  testthat::expect_equal(round(SWC(subject_1, trial_1, metric_1, effect_size = 0.2, method = 'AVG')[1, 1], 2), 0.09)
 })
 
 testthat::test_that("SWC", {
-  testthat::expect_equal(round(SWC(subject_2, trial_2, metric_2), 2)[1, 1], 0.03)
+  testthat::expect_equal(round(SWC(subject_2, trial_2, metric_2, effect_size = 0.2, method = 'AVG')[1, 1], 2), 0.03)
 })
